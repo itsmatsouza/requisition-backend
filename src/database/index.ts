a66ae53ef666492c965f7feddbede96320c36353
itsmatsouza <mateus.h1055@gmail.com>
2023-06-19 00:49:14 -0300

    changing path

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost',
+  host: 'https://requisition-workflow-initial.onrender.com',
   port: 5432,
   database: 'requisition_development',
   username: 'postgres',
